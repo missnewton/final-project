3a7cce1d15f066bec0fd1567f27c648f1e20d31b
ksenia-5 <xenia.germanovich@gmail.com>
2023-06-27 21:55:09 +0100

    prettier css

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-// function signUpForm() {
-//     // console.log("Enter your details");
-//     let form = document.getElementById("signup-form");
-// }
-
 function show_bio(id) {
     /* Function takes id and reveals the bio for that id */
     console.log("Bio coming");
@@ -10,6 +5,3 @@ function show_bio(id) {
     bio_div.setAttribute('style','display:contents');
 
 }
-
-// const shelley_button = document.getElementById("more_shelley");
-// shelley_button.addEventListener("click", console.log("I'm shelley"));
