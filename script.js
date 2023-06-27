587fe8c108c7bb554a29612dece4e1fa189fc095
ksenia-5 <xenia.germanovich@gmail.com>
2023-06-27 21:32:59 +0100

    Bio shown when person's name is clicked

--- script.js
+++ script.js
@@ -2,3 +2,14 @@
 //     // console.log("Enter your details");
 //     let form = document.getElementById("signup-form");
 // }
+
+function show_bio(id) {
+    /* Function takes id and reveals the bio for that id */
+    console.log("Bio coming");
+    let bio_div = document.getElementById(id)
+    bio_div.setAttribute('style','display:contents');
+
+}
+
+// const shelley_button = document.getElementById("more_shelley");
+// shelley_button.addEventListener("click", console.log("I'm shelley"));
